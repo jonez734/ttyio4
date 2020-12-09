13731fd95c19f1c8413d6123ad24a16909e4cc30
Jeff MacDonald <jam@zoidtechnologies.com>
2020-12-08 20:38:30 -0500

    ttyio4/testlexer.py: added handling of }} (CLOSEBRACE) and {{ (OPENBRACE); when handling F6, use group 5 instead of group 3; tweaked regexp for WORD to break on '{' and '}'

--- testlexer.py
+++ testlexer.py
@@ -8,21 +8,20 @@ class Token(NamedTuple):
 def tokenize(buf):
     terminalwidth = 80
 
-    # keywords = {'IF', 'THEN', 'ENDIF', 'FOR', 'NEXT', 'GOSUB', 'RETURN'}
     token_specification = [
-#        ("F6",         r'{F6:(\d)}'),
+        ("OPENBRACE",  r'\{\{'),
+        ("CLOSEBRACE", r'\}\}'),
         ("F6",         r'\{F6(:(\d))?\}'),
         ("WHITESPACE", r'[ \t\n]+'), # iswhitespace()
         ("COMMAND",    r'\{[^\}]+\}'),     # {red}, {brightyellow}, etc
-        ("WORD",       r'[^ \t\n\{]+'),
+        ("WORD",       r'[^ \t\n\{\}]+'),
         ('MISMATCH',   r'.')            # Any other character
     ]
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     for mo in re.finditer(tok_regex, buf):
-        # print(mo)
         kind = mo.lastgroup
+        print("kind=%r mo.groups()=%r" % (kind, mo.groups()))
         value = mo.group()
-        # column = mo.start() - line_start
         if kind == "WHITESPACE":
             pass
         elif kind == "COMMAND":
@@ -30,10 +29,10 @@ def tokenize(buf):
         elif kind == "WORD":
             pass
         elif kind == "F6":
-            if mo.group(3) is None:
+            if mo.group(5) is None:
                 value = 1
             else:
-                value = int(mo.group(3))
+                value = int(mo.group(5))
         elif kind == 'MISMATCH':
             pass
             # raise RuntimeError(f'{value!r} unexpected on line {line_num}')
@@ -42,7 +41,7 @@ def tokenize(buf):
 terminalwidth = 80
 # buf = '''          {red}foo bar {green}baz     !{F6}here is another line{yellow}with a yellow ending.'''
 
-buf = """{F6:2}     {white}Lorem ipsum dolor sit amet,{F6}consectetur adipiscing elit. Qu{blink}isqu{/blink}e ac risus tempor, {orange}porttitor diam id, ullamcorper ipsum. Maecenas quis sapien eget massa finibus auctor. Aliquam purus ligula, commodo ac tellus vitae, tincidunt molestie nisi. Duis dapibus sit amet enim laoreet rutrum. Nunc in sem posuere, tincidunt mi vitae, bibendum nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Vestibulum molestie elementum sapien non consectetur. Praesent in auctor felis. Ut nec eros ac nunc porttitor aliquam id ornare ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet id lorem sit amet sollicitudin. Maecenas sollicitudin efficitur leo, quis vulputate metus bibendum ornare. Fusce vel ex vehicula libero scelerisque lobortis. Etiam at nibh purus. Aliquam ac neque volutpat, condimentum nibh nec, eleifend dolor. Aenean feugiat, mauris at tempor convallis, leo neque feugiat arcu, sed tincidunt ipsum felis et ligula. Vivamus sed consectetur ipsum. Donec augue velit, maximus in libero accumsan, hendrerit blandit mauris. Nunc lobortis faucibus accumsan. Maecenas varius arcu ut bibendum ultrices. Sed semper scelerisque lorem, sed vulputate nisi rutrum at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec et suscipit elit. Cras hendrerit mauris ut odio faucibus, eget maximus elit auctor. """
+buf = """{F6:2}     {white}Lorem {{ipsum}} dolor sit amet,{F6}consectetur adipiscing elit. Qu{blink}isqu{/blink}e ac risus tempor, {orange}porttitor diam id, ullamcorper ipsum. Maecenas quis sapien eget massa finibus auctor. Aliquam purus ligula, commodo ac tellus vitae, tincidunt molestie nisi. Duis dapibus sit amet enim laoreet rutrum. Nunc in sem posuere, tincidunt mi vitae, bibendum nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Vestibulum molestie elementum sapien non consectetur. Praesent in auctor felis. Ut nec eros ac nunc porttitor aliquam id ornare ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer imperdiet id lorem sit amet sollicitudin. Maecenas sollicitudin efficitur leo, quis vulputate metus bibendum ornare. Fusce vel ex vehicula libero scelerisque lobortis. Etiam at nibh purus. Aliquam ac neque volutpat, condimentum nibh nec, eleifend dolor. Aenean feugiat, mauris at tempor convallis, leo neque feugiat arcu, sed tincidunt ipsum felis et ligula. Vivamus sed consectetur ipsum. Donec augue velit, maximus in libero accumsan, hendrerit blandit mauris. Nunc lobortis faucibus accumsan. Maecenas varius arcu ut bibendum ultrices. Sed semper scelerisque lorem, sed vulputate nisi rutrum at. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec et suscipit elit. Cras hendrerit mauris ut odio faucibus, eget maximus elit auctor. """
 
 pos = 0
 result = ""
