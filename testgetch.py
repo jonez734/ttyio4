2a5f1f4a383b1674c69749ad085c29c1d2fec42a
Jeff MacDonald <jam@zoidtechnologies.com>
2021-03-03 21:32:51 -0500

    ttyio4/testgetch.py: added test_pauseabort() and a call to it.

--- testgetch.py
+++ testgetch.py
@@ -1,11 +1,17 @@
 import ttyio4 as ttyio
 
-for x in range(0, 50):
-    ttyio.echo("x=%r" % (x))
-    ch = ttyio.getch(noneok=True)
-    if ch == "/":
-        ttyio.echo("slash detected")
-        break
-    elif ch == " ":
-        ttyio.inputchar("*paused*", " ", "")
-        ttyio.echo("{CHA}{EL}", end="")
+def test_pauseabort():
+    for x in range(0, 50):
+        ttyio.echo("x=%r" % (x))
+        ch = ttyio.getch(noneok=True)
+        if ch == "/":
+            ttyio.echo("slash detected")
+            break
+        elif ch == " ":
+            ttyio.inputchar("*paused*", " ", "")
+            ttyio.echo("{CHA}{EL}", end="")
+
+try:
+    test_pauseabort()
+except KeyboardInterrupt:
+    ttyio.echo("INTR")
