a77f27af9303354ab4f4af93dac52b99cbf09bf7
Jeff MacDonald <jam@zoidtechnologies.com>
2021-03-07 21:01:49 -0500

    ttyio4/testaltcharset.py: add call to bbsengine.title(); tweak echo() call to output only one hline char.

--- testaltcharset.py
+++ testaltcharset.py
@@ -1,6 +1,9 @@
 import ttyio4 as ttyio
+import bbsengine5 as bbsengine
 
 # https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#designate-character-set
 # ttyio.echo("\033(0l{white}qqqqq{lightblue}qqqqqq{lightred}qqqk\033(B abcdefg!{/all}")
-ttyio.echo("{lightred}{acs:hline:10}{/all}this is a test")
+ttyio.echo("{lightred}{acs:hline}{/all}this is a test")
 
+
+bbsengine.title("lorem ipsum lorem ipsum")
