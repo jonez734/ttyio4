4d9b66730e6a32d92c060d10e59b5ae835f748af
Jeff MacDonald <jam@zoidtechnologies.com>
2021-07-15 21:57:41 -0400

    ttyio4/testunicode.py: revised to test {unicode} *and* {u}.

--- testunicode.py
+++ testunicode.py
@@ -1,14 +1,14 @@
 import ttyio4 as ttyio
 
 def main():
-    ttyio.echo("{unicode:heart:10}{unicode:diamond:2}")
-    ttyio.echo("{unicode:dv}{unicode:sv:42}")
-    ttyio.echo("{unicode:heart}{unicode:diamond}{unicode:club}{unicode:spade}")
-    ttyio.echo("{unicode:drdvcorner}{unicode:dhline:10}{unicode:dldvcorner}")
-    ttyio.echo("{unicode:dvline}          {unicode:dvline}")
-    ttyio.echo("{unicode:dvdhrtee}{unicode:dhline:10}{unicode:dvdhltee}")
-    ttyio.echo("{unicode:dvshrtee}{acs:hline:10}{unicode:dvshltee}")
-    ttyio.echo("{unicode:dvdrcorner}{unicode:dhline:10}{unicode:dvdlcorner}") # {unicode:dvdhcross}")
+    ttyio.echo("{u:heart:10}{u:diamond:2}")
+    ttyio.echo("{u:dv}{u:sv:42}")
+    ttyio.echo("{u:heart}{u:diamond}{u:club}{u:spade}")
+    ttyio.echo("{u:drdvcorner}{u:dhline:10}{u:dldvcorner}")
+    ttyio.echo("{u:dvline}          {u:dvline}")
+    ttyio.echo("{u:dvdhrtee}{u:dhline:10}{u:dvdhltee}")
+    ttyio.echo("{u:dvshrtee}{acs:hline:10}{u:dvshltee}")
+    ttyio.echo("{unicode:dvdrcorner}{u:dhline:10}{u:dvdlcorner}") # {unicode:dvdhcross}")
     return
 
 if __name__ == "__main__":
