c205700a3bef5d8c258ab73dc1849c1dc6c09cd0
Jeff MacDonald <jam@zoidtechnologies.com>
2021-07-29 21:56:55 -0400

    ttyio4/ttyio4.py: updated test to use a few nesting levels of {var}s, and it works nicely.

--- testvar.py
+++ testvar.py
@@ -1,7 +1,10 @@
 import ttyio4 as ttyio
 
-ttyio.setvariable("test", 42)
-buf = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. {green}{var:test}{/all}At elementum eu facilisis sed odio morbi. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Nunc mattis enim ut tellus. Faucibus purus in massa tempor nec feugiat. Diam phasellus vestibulum lorem sed risus ultricies tristique. Euismod quis viverra nibh cras. Amet massa vitae tortor condimentum lacinia. Eu scelerisque felis imperdiet proin. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Eleifend quam adipiscing vitae proin sagittis. Mattis vulputate enim nulla aliquet porttitor lacus. Neque aliquam vestibulum morbi blandit. Nisi porta lorem mollis aliquam ut porttitor.
+ttyio.setvariable("foo", "{var:color}")
+ttyio.setvariable("color", "{green}")
+ttyio.setvariable("test", "{var:foo}eggs")
+buf = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. {var:test}{/all}At elementum eu facilisis sed odio morbi. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Nunc mattis enim ut tellus. Faucibus purus in massa tempor nec feugiat. Diam phasellus vestibulum lorem sed risus ultricies tristique. Euismod quis viverra nibh cras. Amet massa vitae tortor condimentum lacinia. Eu scelerisque felis imperdiet proin. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Eleifend quam adipiscing vitae proin sagittis. Mattis vulputate enim nulla aliquet porttitor lacus. Neque aliquam vestibulum morbi blandit. Nisi porta lorem mollis aliquam ut porttitor.
 """
 
-ttyio.echo(buf)
+ttyio.echo(buf, wordwrap=True)
